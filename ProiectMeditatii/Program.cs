7509ba5a7ff9b58b250035b71f65047cf833d5c7
Vlad Zaharia <vlad.zaharia95@gmail.com>
2022-02-14 20:52:35 +0200

    initial commit

---
+++ ProiectMeditatii/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectMeditatii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrie in console
+            Console.WriteLine("Vlad");
+
+            int number = 4;
+
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+
+        }
+    }
+}
